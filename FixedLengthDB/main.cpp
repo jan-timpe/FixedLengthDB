dd760886a37958901694045297be633b9082b688
Jan Timpe <jantimpe@email.uark.edu>
2017-01-22 13:16:05 -0600

    Initial Commit

---
+++ FixedLengthDB/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FixedLengthDB
+//
+//  Created by Jan Timpe on 1/22/17.
+//  Copyright © 2017 Jan Timpe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
